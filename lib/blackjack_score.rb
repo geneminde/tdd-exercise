38d2aeb19ed5625aace0234ac8123d0b5a63b98b
Genevieve Neely <genevieve.hood@gmail.com>
2020-09-14 21:22:23 -0700

    finished assignment

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -2,6 +2,54 @@
 
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
+def tally_cards(hand)
+  score = 0
+  hand.each do |card|
+    if card.class == String
+      if %w[Jack Queen King].include?(card.capitalize)
+        score += 10
+      end
+    else
+      score += card
+    end
+  end
+  return score
+end
+
+def valid_hand(hand)
+  valid_cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
+  hand.each do |element|
+    raise ArgumentError if !valid_cards.include?(element)
+  end
+  raise ArgumentError if hand.length == 0 || hand.length > 5
+end
+
 def blackjack_score(hand)
+  score = 0
+  valid_hand(hand)
+  if hand.include?('Ace')
+    if hand.count('Ace') == 1
+      hand.delete('Ace')
+      if tally_cards(hand) <= 10
+        score = tally_cards(hand) + 11
+      else
+        score = tally_cards(hand) + 1
+      end
+    else
+      aces = hand.count('Ace')
+      hand.delete('Ace')
+      if (tally_cards(hand) + aces - 1) > 10
+        score = tally_cards(hand) + aces
+      else
+        score = tally_cards(hand) + aces + 10
+      end
+    end
+  else
+    score = tally_cards(hand)
+  end
+
+  raise ArgumentError if score > 21
+  return score
 
 end
+
--- test/blackjack_score_test.rb
+++ test/blackjack_score_test.rb
@@ -18,26 +18,93 @@ describe 'Blackjack Score' do
     score = blackjack_score(hand)
 
     # Assert <-  You do this part!
+    expect(score).must_equal 7
 
   end
 
   it 'facecards have values calculated correctly' do
+    hand = ['King', 'Queen']
+    score = blackjack_score(hand)
+    expect(score).must_equal 20
 
+    hand = ['Jack', 2, 9]
+    score = blackjack_score(hand)
+    expect(score).must_equal 21
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
+    hand = ['Ace', 2, 3]
+    score = blackjack_score(hand)
+    expect(score).must_equal 16
+
+    hand = ['King', 'Ace']
+    score = blackjack_score(hand)
+    expect(score).must_equal 21
 
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
+    hand = ['Ace', 9, 9]
+    score = blackjack_score(hand)
+    expect(score).must_equal 19
+
+    hand = ['Ace', 'Ace', 9, 9]
+    score = blackjack_score(hand)
+    expect(score).must_equal 20
+
+    hand = ['Jack', 'Queen', 'Ace']
+    score = blackjack_score(hand)
+    expect(score).must_equal 21
+
+    hand = ['Ace', 'Ace', 'Ace', 'Ace']
+    score = blackjack_score(hand)
+    expect(score).must_equal 14
 
   end
 
   it 'raises an ArgumentError for invalid cards' do
+    hand = [7, 2, 'ace']
+    expect{
+      blackjack_score(hand)
+    }.must_raise ArgumentError
+
+    hand = []
+    expect{
+      blackjack_score(hand)
+    }.must_raise ArgumentError
+
+    hand = [1, 2, 3, 4, 5]
+    expect{
+      blackjack_score(hand)
+    }.must_raise ArgumentError
+
+    hand = [2, 2, 2, 2, 2, 2]
+    expect{
+      blackjack_score(hand)
+    }.must_raise ArgumentError
 
   end
 
   it 'raises an ArgumentError for scores over 21' do
+    hand = ['King', 'King', 'King']
+    expect{
+      blackjack_score(hand)
+    }.must_raise ArgumentError
+
+    hand = [10, 'King', 'Ace', 'Ace']
+    expect{
+      blackjack_score(hand)
+    }.must_raise ArgumentError
+
+    hand = ['Ace', 10, 10, 10]
+    expect{
+      blackjack_score(hand)
+    }.must_raise ArgumentError
+
+    hand = [8, 8, 8]
+    expect{
+      blackjack_score(hand)
+    }.must_raise ArgumentError
 
   end
 end
