f2724fbc7966457bb2370411d048c7df62511f05
Genevieve Neely <genevieve.hood@gmail.com>
2020-10-05 18:55:58 -0700

    add test_helper.rb

---
+++ test/test_helper.rb
@@ -0,0 +1,11 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter 'test/'
+end
+require 'minitest'
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/pride'
+
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
